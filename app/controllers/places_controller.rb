28fd14a96b61cf6c30b6dc8cb8095e0bee175fd3
Jankikellogg <janki.somani@kellogg.northwestern.edu>
2026-05-22 03:08:21 +0000

    Final Edits

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,31 @@
+class PlacesController < ApplicationController
+
+  def index
+
+    @places = Place.all
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+    @place = Place.new
+
+    @place["name"] = params["name"]
+
+    @place.save
+
+    redirect_to "/places"
+
+  end
+
+  def show
+
+    @place = Place.find(params["id"])
+
+  end
+
+end
